8c866ba81855db2752bf9fe7fc6c335be747391c
SamuelTownsend <91908426+SamuelTownsend@users.noreply.github.com>
2023-05-25 14:49:41 -0500

    updating test file for task_0

--- React_component/task_0/dashboard/src/App/app.test.js
+++ React_component/task_0/dashboard/src/App/app.test.js
@@ -11,18 +11,34 @@ describe('<App />', () => {
 		expect(wrapper.exists()).toBe(true);
 	});
 
-	it('App-header renders', () => {
+	it('App renders Login when isLoggedIn=false', () => {
 		const wrapper = shallow(<App />);
-		expect(wrapper.find('.App-header').exists()).toBe(true);
+		expect(wrapper.find('CourseList').length).toBe(0);
 	});
 
-	it('App-body renders', () => {
-		const wrapper = shallow(<App />);
-		expect(wrapper.find('.App-body').exists()).toBe(true);
+	it('App renders CourseList when isLoggedIn=true', () => {
+		const wrapper = shallow(<App isLoggedIn={true}/>);
+		expect(wrapper.find('Login').length).toBe(0);
+		expect(wrapper.find('CourseList').length).toBe(1);
 	});
 
-	it('App-footer renders', () => {
+	it('Notifications renders', () => {
 		const wrapper = shallow(<App />);
-		expect(wrapper.find('.App-footer').exists()).toBe(true);
+		expect(wrapper.find('Notifications').exists()).toBe(true);
+	});
+
+	it('Header renders', () => {
+		const wrapper = shallow(<App />);
+		expect(wrapper.find('Header').exists()).toBe(true);
+	});
+
+	it('Login renders', () => {
+		const wrapper = shallow(<App />);
+		expect(wrapper.find('Login').exists()).toBe(true);
+	});
+
+	it('Footer renders', () => {
+		const wrapper = shallow(<App />);
+		expect(wrapper.find('Footer').exists()).toBe(true);
 	});
 })
