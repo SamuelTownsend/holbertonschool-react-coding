7f6a50cc90265c90ee53c19bfef3a8f2b74208c6
SamuelTownsend <91908426+SamuelTownsend@users.noreply.github.com>
2023-05-25 14:48:06 -0500

    adding task 1

--- React_component/task_1/dashboard/src/App/App.test.js
+++ React_component/task_1/dashboard/src/App/App.test.js
@@ -2,6 +2,10 @@ import App from './App';
 import React from 'react';
 import { shallow, configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import 'jsdom-global/register';
+import sinon from 'sinon';
+
+window.alert = sinon.spy()
 
 configure({adapter: new Adapter()});
 
@@ -9,6 +13,17 @@ describe('<App />', () => {
 	it('App renders w/o crashing', () => {
 		const wrapper = shallow(<App />);
 		expect(wrapper.exists()).toBe(true);
+	});
+
+	it('App renders Login when isLoggedIn=false', () => {
+		const wrapper = shallow(<App />);
+		expect(wrapper.find('CourseList').length).toBe(0);
+	});
+
+	it('App renders CourseList when isLoggedIn=true', () => {
+		const wrapper = shallow(<App isLoggedIn={true}/>);
+		expect(wrapper.find('Login').length).toBe(0);
+		expect(wrapper.find('CourseList').length).toBe(1);
 	});
 
 	it('Notifications renders', () => {
